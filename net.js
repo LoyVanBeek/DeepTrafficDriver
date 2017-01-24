dc8c5105a470ab78c9679d0bcd54f3c24f6c87f7
Loy <LoyVanBeek@users.noreply.github.com>
2017-01-24 10:26:18 +0100

    Slightly larger temporal window: 10. Score: 68.98 MpH

--- net.js
+++ net.js
@@ -8,7 +8,7 @@ trainIterations = 40000;
 
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
-var temporal_window = 6;
+var temporal_window = 8;
 var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
 
 var layer_defs = [];
